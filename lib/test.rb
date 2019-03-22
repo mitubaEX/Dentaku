24571b17acb13772692dd311ac39700c8016438b
mitubaEX <jun-nakamura@c-fo.com>
2019-03-22 12:47:07 +0900

    add parsing expression

--- lib/test.rb
+++ lib/test.rb
@@ -6,11 +6,81 @@ module Test
   # Your code goes here...
   class Calculator
     def initialize(word)
-      @word = word
+      @words = word.split("")
+      @current_index = 0
+      @previous_index = 0
     end
 
     def eval
-      @word
+      value = 0
+      while @current_index != @words.length do
+        skip_whitespace
+        current_word = @words[@current_index]
+
+        if is_digit?(current_word)
+          value = parse_digit
+
+        elsif is_plus?(current_word)
+          @current_index += 1
+          value = value + self.eval
+
+        elsif is_minus?(current_word)
+          @current_index += 1
+          value = value - self.eval
+
+        elsif is_multi?(current_word)
+          @current_index += 1
+          value = value * self.eval
+
+        elsif is_div?(current_word)
+          @current_index += 1
+          value = value / self.eval
+
+        elsif is_mod?(current_word)
+          @current_index += 1
+          value = value % self.eval
+        end
+      end
+      value
+    end
+
+    def is_plus?(word)
+      word == '+'
+    end
+    def is_minus?(word)
+      word == '-'
+    end
+    def is_multi?(word)
+      word == '*'
+    end
+    def is_div?(word)
+      word == '/'
+    end
+    def is_mod?(word)
+      word == '%'
+    end
+
+    def parse_digit
+      num = 0
+      while is_digit?(@words[@current_index]) do
+        num = num * 10 + @words[@current_index].to_i
+        @current_index += 1
+      end
+      num
+    end
+
+    def is_digit?(word)
+      (word =~ /^[0-9]$/) == 0
+    end
+
+    def skip_whitespace
+      while is_whitespace? do
+        @current_index += 1
+      end
+    end
+
+    def is_whitespace?
+      @words[@current_index] == ' '
     end
   end
 end
--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,9 +1,64 @@
 RSpec.describe Test do
-  it "input single number" do
-    expect(Test::Calculator.new("1").eval).to eq("1")
-  end
 
-  it "input double number" do
-    expect(Test::Calculator.new("12").eval).to eq("12")
+  describe 'input number' do
+    subject(:calculator) { Test::Calculator.new(params).eval }
+
+    context 'when input single number' do
+      context 'with single number' do
+        let(:params) { '1' }
+        it 'single number' do
+          is_expected.to eq(1)
+        end
+      end
+
+      context 'when single number with whitespace' do
+        let(:params) { '        1' }
+        it 'single number with whitespace' do
+          is_expected.to eq(1)
+        end
+      end
+    end
+
+
+    context 'when input multiple number' do
+      context 'with double number' do
+        let(:params) { '12' }
+        it 'double number' do
+          is_expected.to eq(12)
+        end
+      end
+
+      context 'with triple number' do
+        let(:params) { '123' }
+        it 'triple number' do
+          is_expected.to eq(123)
+        end
+      end
+    end
+
+    context 'when input single expression' do
+      context 'with double number' do
+        let(:params) { '1+2' }
+        it 'double number' do
+          is_expected.to eq(3)
+        end
+      end
+
+      context 'with double number with whitespace' do
+        let(:params) { '   1   +   2   ' }
+        it 'double number with whitespace' do
+          is_expected.to eq(3)
+        end
+      end
+    end
+
+    context 'when input multiple expression' do
+      context 'with triple number' do
+        let(:params) { '1 + 2 + 3' }
+        it 'triple number' do
+          is_expected.to eq(6)
+        end
+      end
+    end
   end
 end
